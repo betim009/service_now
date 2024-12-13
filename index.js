470840859d8a5bc64b13cf38b13c59fb39e8f9b9
unknown <betim009@gmail.com>
2024-12-13 20:23:36 -0300

    update

---
+++ index.js
@@ -0,0 +1,16 @@
+/*
+O que é o service now?
+é uma plataforma web, que processa e gerencia dados em formato de planilhas.
+
+funcionalidades:
+- Criar novas interfaces
+- Criar filtros personalizados
+- Combinar filtros
+
+
+
+- 
+-
+- 
+
+*/
